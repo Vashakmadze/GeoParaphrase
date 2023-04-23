d6f90343eab91d86f6830e45d95443b61d9cf1e4
luka.vashakmadze <luka.vashakmadze@student.howest.be>
2023-04-23 19:22:48 +0200

    analytics import

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,6 +3,7 @@ import Header from '../components/Header'
 import Paraphraser from '../components/Paraphraser'
 import About from '../components/About'
 import Footer from '../components/Footer'
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Paraphraser />
       <About />
       <Footer />
+      <Analytics />
     </>
   )
 }
