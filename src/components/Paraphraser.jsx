6e815867912dc9bd0013ff99e75e4da9142159a5
luka.vashakmadze <luka.vashakmadze@student.howest.be>
2023-04-23 22:09:26 +0200

    removed dark mode style changes

--- src/components/Paraphraser.jsx
+++ src/components/Paraphraser.jsx
@@ -71,7 +71,7 @@ function Paraphraser() {
     <main className='w-10/12 m-auto px-3 my-[4rem]'>
       <div className='paraphraser flex flex-col lg:flex-row'>
         <div className='relative textarea flex-1 mb-6 lg:mr-6'>
-          <label htmlFor="text" className="block mb-2 font-medium text-gray-900 dark:text-white text-lg">თქვენი ტექსტი <span className='max float-right'>{charlength} / 2000</span></label>
+          <label htmlFor="text" className="block mb-2 font-medium text-gray-90 text-lg">თქვენი ტექსტი <span className='max float-right'>{charlength} / 2000</span></label>
           <textarea ref={inputRef} maxLength={2000} id="text" rows="10" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 focus:outline-blue-400" placeholder="შეიყვანეთ თქვენი ტექსტი აქ..." onChange={updateLength}>
           </textarea>
           <button className="bg-[#60A5FA] hover:bg-blue-500 text-white font-bold py-2 px-4 rounded absolute bottom-5 right-5" onClick={paraphrase}>
@@ -79,7 +79,7 @@ function Paraphraser() {
           </button>
         </div>
         <div className='textarea flex-1'>
-          <label htmlFor="text-output" className="block mb-2 text-lg font-medium text-gray-900 dark:text-white">პერიფრაზირებული ტექსტი</label>
+          <label htmlFor="text-output" className="block mb-2 text-lg font-medium text-gray-900">პერიფრაზირებული ტექსტი</label>
           <textarea ref={outputRef} id="text-putput" rows="10" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 focus:outline-blue-400" placeholder="აქ გამოისახება თქვენი ტექსტი..." readOnly></textarea>
         </div>
       </div>
