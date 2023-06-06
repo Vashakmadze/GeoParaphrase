862f046199e778c7ac81e1c8c182a34ffb38de2b
luka.vashakmadze <geogamerstime@gmail.com>
2023-06-06 18:36:19 +0200

    removed dark mode

--- src/components/PriceCard.jsx
+++ src/components/PriceCard.jsx
@@ -2,18 +2,18 @@ import React from "react";
 
 function PriceCard({ data }) {
 	return (
-		<div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-			<h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">
+		<div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8">
+			<h5 className="mb-4 text-xl font-medium text-gray-500">
 				{data.name}
 			</h5>
-			<div className="flex items-baseline text-gray-900 dark:text-white">
+			<div className="flex items-baseline text-gray-900">
 				<span className="text-4xl font-semibold font-sans translate-y-[-15%] mr-1">
 					$
 				</span>
 				<span className="text-5xl font-extrabold tracking-wider">
 					{data.price}
 				</span>
-				<span className="ml-1 text-xl font-normal text-gray-500 dark:text-gray-400">
+				<span className="ml-1 text-xl font-normal text-gray-500>
 					/month
 				</span>
 			</div>
@@ -23,7 +23,7 @@ function PriceCard({ data }) {
 				<li className="flex space-x-3">
 					<svg
 						aria-hidden="true"
-						className="flex-shrink-0 w-5 h-5 text-blue-600 dark:text-blue-500"
+						className="flex-shrink-0 w-5 h-5 text-blue-600"
 						fill="currentColor"
 						viewBox="0 0 20 20"
 						xmlns="http://www.w3.org/2000/svg">
@@ -33,14 +33,14 @@ function PriceCard({ data }) {
 							d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
 							clipRule="evenodd"></path>
 					</svg>
-					<span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
+					<span className="text-base font-normal leading-tight text-gray-500">
 						მაქსიმუმ {data.offers.charachters} ასო თითო გადაწერაში
 					</span>
 				</li>
 				<li className="flex space-x-3">
 					<svg
 						aria-hidden="true"
-						className="flex-shrink-0 w-5 h-5 text-blue-600 dark:text-blue-500"
+						className="flex-shrink-0 w-5 h-5 text-blue-600"
 						fill="currentColor"
 						viewBox="0 0 20 20"
 						xmlns="http://www.w3.org/2000/svg">
@@ -50,14 +50,14 @@ function PriceCard({ data }) {
 							d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
 							clipRule="evenodd"></path>
 					</svg>
-					<span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
+					<span className="text-base font-normal leading-tight text-gray-500">
 						მაქსიმუმ {data.offers.words} სიტყვა თითო გადაწერაში.
 					</span>
 				</li>
 				<li className="flex space-x-3">
 					<svg
 						aria-hidden="true"
-						className="flex-shrink-0 w-5 h-5 text-blue-600 dark:text-blue-500"
+						className="flex-shrink-0 w-5 h-5 text-blue-600"
 						fill="currentColor"
 						viewBox="0 0 20 20"
 						xmlns="http://www.w3.org/2000/svg">
@@ -67,7 +67,7 @@ function PriceCard({ data }) {
 							d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
 							clipRule="evenodd"></path>
 					</svg>
-					<span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
+					<span className="text-base font-normal leading-tight text-gray-500">
 						დღეში {data.offers.paraphrases} გადაწერა.
 					</span>
 				</li>
@@ -91,7 +91,7 @@ function PriceCard({ data }) {
 			</ul>
 			<button
 				type="button"
-				className="text-white bg-gray-600 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-gray-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center">
+				className="text-white bg-gray-600 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-blue-200 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center">
 				მალე
 				{/* გაწევრიანდი */}
 			</button>
