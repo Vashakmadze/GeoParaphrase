9d9c3eff24ff43df115ba478b51db9e6111ac865
luka.vashakmadze <geogamerstime@gmail.com>
2023-09-16 17:56:04 +0200

    changed checkmark colors

--- src/components/PriceCard.jsx
+++ src/components/PriceCard.jsx
@@ -30,7 +30,7 @@ function PriceCard({ data, signedIn }) {
 				<li className="flex space-x-3">
 					<svg
 						aria-hidden="true"
-						className="flex-shrink-0 w-5 h-5 text-indigo-700"
+						className="flex-shrink-0 w-5 h-5 text-blue-500"
 						fill="currentColor"
 						viewBox="0 0 20 20"
 						xmlns="http://www.w3.org/2000/svg">
@@ -47,7 +47,7 @@ function PriceCard({ data, signedIn }) {
 				<li className="flex space-x-3">
 					<svg
 						aria-hidden="true"
-						className="flex-shrink-0 w-5 h-5 text-indigo-700"
+						className="flex-shrink-0 w-5 h-5 text-blue-500"
 						fill="currentColor"
 						viewBox="0 0 20 20"
 						xmlns="http://www.w3.org/2000/svg">
@@ -64,7 +64,7 @@ function PriceCard({ data, signedIn }) {
 				<li className="flex space-x-3">
 					<svg
 						aria-hidden="true"
-						className="flex-shrink-0 w-5 h-5 text-indigo-700"
+						className="flex-shrink-0 w-5 h-5 text-blue-500"
 						fill="currentColor"
 						viewBox="0 0 20 20"
 						xmlns="http://www.w3.org/2000/svg">
@@ -106,7 +106,7 @@ function PriceCard({ data, signedIn }) {
 				<button
 					onClick={buy}
 					type="button"
-					className="text-white bg-teal-600 hover:bg-teal-700 focus:ring-4 focus:outline-none focus:ring-blue-200 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center">
+					className="text-white bg-[#60A5FA] hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-200 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center">
 					გაწევრიანდი
 				</button>
 			)}
