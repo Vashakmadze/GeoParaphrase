42efa62e72cf326689da65582414794090864710
luka.vashakmadze <geogamerstime@gmail.com>
2023-11-23 02:20:52 +0100

    added https aws

--- src/services/summarize.js
+++ src/services/summarize.js
@@ -1,5 +1,5 @@
 const summarizeMain = async (message, setLoading) => {
-    const url = "https://gadawre-server.vercel.app/summarize";
+    const url = "https://3.91.197.39:8000/summarize";
 
     const apiRequestBody = {
         "text": message
