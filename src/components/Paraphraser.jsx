a2d097f37b5d1319d91f1caa3ae732a96a7ae65f
luka.vashakmadze <geogamerstime@gmail.com>
2023-12-05 01:52:18 +0100

    using useparaphraser hook

--- src/components/Paraphraser.jsx
+++ src/components/Paraphraser.jsx
@@ -1,63 +1,11 @@
 import React, { useRef, useState } from "react";
 import paraphraseMain from "../services/paraphrase";
 import { LoaderSmall } from "./Loader";
+import useParaphraser from "../hooks/useParaphraser";
 
 function Paraphraser({ tier }) {
-	const [charlength, setCharlength] = useState(0);
-	const [loading, setLoading] = useState(false);
-	const date = new Date().toLocaleDateString("en-US");
-	const inputRef = useRef(null);
-	const outputRef = useRef(null);
-
-	const updateLength = (e) => {
-		setCharlength(e.target.value.length);
-	};
-
-	const paraphrasedResult = async () => {
-		const text = inputRef.current.value;
-
-		if (text.length > 1) {
-			const finalResult = await paraphraseMain(text, setLoading);
-
-			outputRef.current.value = finalResult.data;
-		}
-	};
-
-	const updateDailyUsage = () => {
-		const usage = JSON.parse(localStorage.getItem("usage"));
-		usage[date]++;
-		localStorage.setItem("usage", JSON.stringify(usage));
-	};
-
-	const createDailyUsage = () => {
-		const usageObject = {
-			[date]: 0,
-		};
-		localStorage.setItem(`usage`, JSON.stringify(usageObject));
-	};
-
-	const paraphrase = () => {
-		const usage = JSON.parse(localStorage.getItem("usage"));
-		if (usage) {
-			if (Object.keys(usage)[0] === date) {
-				if (usage[date] < tier.maxParaphrases) {
-					paraphrasedResult();
-					updateDailyUsage();
-				} else {
-					alert(
-						"თქვენ დღიური ლიმიტი ამოგეწურათ. ამჟამინდელი ყოველდღიური ლიმიტია - " +
-							tier.maxParaphrases
-					);
-				}
-			} else {
-				createDailyUsage();
-				paraphrasedResult();
-			}
-		} else {
-			createDailyUsage();
-			paraphrasedResult();
-		}
-	};
+	const { charlength, loading, inputRef, outputRef, updateLength, paraphrase } =
+		useParaphraser(tier);
 
 	return (
 		<div className="w-10/12 m-auto px-3 my-[4rem] selection:bg-blue-400">
